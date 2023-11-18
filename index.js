ad0b5e501a9525c17641439a66884c3672d9cf03
Divine <divinetidy@gmail.com>
2023-11-18 06:52:10 +0100

    added secret number

--- index.js
+++ index.js
@@ -1,39 +1,40 @@
 "use strict";
 let score = 20 - 1;
+let min = 1;
 let secretnumber = Math.trunc(Math.random() * 29) + 1;
 console.log(secretnumber);
+function displayMassage(massage) {
+  document.querySelector(`.startguessing`).textContent = massage;
+}
 
 document.querySelector(`.check`).addEventListener(`click`, function () {
   let inputnumber = Number(document.querySelector(`.inputnumber`).value);
-  inputnumber = document.querySelector(`.inputnumber`).value;
-  // console.log(inputnumber);
 
   if (!inputnumber) {
-    console.log(inputnumber);
-    // document.querySelector(`.startguessing`).textContent = `not a number`;
+    displayMassage(`Forbidden Number`);
   }
 
   if (inputnumber == secretnumber) {
-    document.querySelector(`.startguessing`).textContent = ` congratulations`;
+    displayMassage(`congratulations`);
     document.querySelector(`.secretnumber`).textContent = ` ${secretnumber}`;
     document.querySelector(`.highscore`).textContent = `${secretnumber}`;
     document.querySelector(`body`).style.backgroundColor = `green`;
     document.querySelector(`.guessmynumber`).textContent = `MTN:123456789`;
   } else if (inputnumber > secretnumber) {
-    document.querySelector(`.startguessing`).textContent = `Too hight`;
+    displayMassage(`Too hight`);
     document.querySelector(`.score`).textContent = `${score--}`;
   } else if (inputnumber < secretnumber) {
-    document.querySelector(`.startguessing`).textContent = `Too low`;
+    displayMassage(`Too low`);
     document.querySelector(`.score`).textContent = `${score--}`;
   }
   if (score < 1) {
     document.querySelector(`.score`).textContent = `0`;
-    document.querySelector(`.startguessing`).textContent = `Game over`;
+    displayMassage(`You lost the game`);
   }
 });
 
 document.querySelector(`.again`).addEventListener(`click`, function () {
-  document.querySelector(`.startguessing`).textContent = `start guessing`;
+  displayMassage(`start guessing`)
   document.querySelector(`.score`).textContent = `20`;
   document.querySelector(`body`).style.backgroundColor = ` rgb(2, 2, 39)`;
   document.querySelector(`.secretnumber`).textContent = `?`;
